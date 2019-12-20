7a76eb5dd5894e4f7f699e57cbf044417f97aeae
Denis Bulanov <BulanovDO@mosreg.ru>
2019-12-20 17:15:52 +0300

    [feature] Remove some classes from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export { SssCore } from './sss-core.class';
-export { ApiRequestManager } from './api/api-request-manager.class';
 export { Auth } from './api/auth.class';
 export { Api } from './api/api.class';
